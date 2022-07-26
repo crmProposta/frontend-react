f0dae8c03735b16039338ed1299179a91663b474
SamuelLimaCap <samueldelimapessoal@gmail.com>
2022-07-26 18:15:11 -0300

    Add a disable event on front-end

--- src/views/master/users/ListAccount/ListAccount.tsx
+++ src/views/master/users/ListAccount/ListAccount.tsx
@@ -1,41 +1,112 @@
-import React, {useEffect, useState} from "react";
-import AppBar from "../../../../components/AppBar";
+import React, { useEffect, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 // @ts-ignore
 import BootstrapTable from 'react-bootstrap-table-next';
+import { toast } from "react-toastify";
+import AppBar from "../../../../components/AppBar";
 import MasterDataSource from "../../../../dataSource/MasterDataSource";
+import { APIError } from "../../../../models/Backend-default/APIError";
+import { APIResponse } from "../../../../models/Backend-default/APIResponse";
+import { Roles, RolesColors } from "../../../../models/Backend-default/Roles";
+import { UserDTO } from "../../../../models/Backend-default/UserDTO";
 import ResponseUtils from "../../../../utils/ResponseUtils";
-import {APIResponse} from "../../../../models/Backend-default/APIResponse";
-import {APIError} from "../../../../models/Backend-default/APIError";
-import {toast} from "react-toastify";
-import {Button} from "react-bootstrap";
-import './index.css'
-import {UserDTO} from "../../../../models/Backend-default/UserDTO";
-import {Roles, RolesColors} from "../../../../models/Backend-default/Roles";
 import RoleUtil from "../../../../utils/RoleUtil";
+import './index.css';
 
 export default function ListAccount() {
 
-    const [products, setProducts] = useState([])
+    const [data, setData] = useState([] as any[])
+    const [disableAccountIdOnClick, setDisableAccountIdOnClick] = useState(-1)
+    
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
     // @ts-ignore
     useEffect(() => {
-        async function getAccounts() {
-            let response: APIResponse<any> | APIError = await MasterDataSource.listAccount()
-            if (ResponseUtils.responseIsNotSuccessful(response.status)) {
-                toast.error((response as APIError).message)
-                return
-            }
-
-            let data = (response as APIResponse<any>).data
-            setProducts(data)
-        }
-
-        getAccounts()
+        getAccountsOnAPI()
 
     }, [])
 
-    const columns = [{
+    async function getAccountsOnAPI() {
+        let response: APIResponse<any> | APIError = await MasterDataSource.listAccount()
+        if (ResponseUtils.responseIsNotSuccessful(response.status)) {
+            toast.error((response as APIError).message)
+            return
+        }
+
+        let data = (response as APIResponse<any>).data
+        setData(data)
+    }
+
+    useEffect(() => {
+        if (disableAccountIdOnClick == null || disableAccountIdOnClick === -1) return
+
+        const dataUpdated = returnDataArrayWithUserDisabledOnFrontEnd((data as UserDTO[]), disableAccountIdOnClick)
+        setData(dataUpdated)
+
+        removeAccountIdOnClick()
+    }, [data, disableAccountIdOnClick])
+
+    function returnDataArrayWithUserDisabledOnFrontEnd(data: UserDTO[], disableAccountIdOnClick: number) {
+        return data.map(account => {
+            if (account.id === disableAccountIdOnClick) {
+                let enabled = account.enabled
+                return { ...account, enabled: !enabled }
+            } else {
+                return account;
+            }
+        })
+    }
+
+    function removeAccountIdOnClick() {
+        setDisableAccountIdOnClick(-1)
+    }
+
+    function modalDisableAccount(id: number): React.MouseEventHandler<HTMLButtonElement> | undefined {
+
+        //TODO: Add a modal do confirm if want to disable/enable the selected account. 
+        //TODO: Show user details on modal
+        setDisableAccountIdOnClick(id)
+
+
+        return undefined
+    }
+
+    const bootstrapTable2ExpandRowListener = {
+        renderer: (row: any) =>
+        (
+            <div>
+                <p>Permissoes:</p>
+                <div className={"ms-5"}>
+                    {returnRolesAsPTag(row.roles as Roles[])}
+                </div>
+            </div>
+        ),
+        showExpandColumn: true,
+        expandByColumnOnly: true,
+        expandColumnRenderer: (expandObj: any) => {
+
+            let color = "light"
+            let text = "+"
+            if (expandObj.expanded)
+                if (expandObj.expanded) {
+                    text = "-"
+                }
+            return (
+                <Button variant={color} size="sm"> {text}</Button>
+            );
+        }
+    };
+
+    const returnRolesAsPTag = (roles: Roles[]) => {
+        let jsxRoles: JSX.Element[] = [];
+        roles.forEach(role => {
+            jsxRoles.push(<p>{role}</p>)
+        })
+
+        return jsxRoles
+    }
+
+    const bootstrapTable2Columns = [{
         dataField: 'id',
         text: 'ID'
     }, {
@@ -55,8 +126,9 @@ export default function ListAccount() {
         formatter: (cellContent: any, row: any) => {
             const user = (row as UserDTO);
             const higherRole = RoleUtil.higherRole((user as UserDTO).roles) as string
-            // @ts-ignore
-            return (<p className={`my-auto text-center rounded rounded-5 text-light bg-${RolesColors[higherRole]}`}>{higherRole}</p>)
+            return (
+                // @ts-ignore
+                <p className={`my-auto text-center rounded rounded-5 text-light bg-${RolesColors[higherRole]}`}>{higherRole}</p>)
         }
     }, {
         dataField: 'editAction',
@@ -70,12 +142,11 @@ export default function ListAccount() {
     }, {
         dataField: 'disableAction',
         text: 'Disable',
-        isDummyField: true,
-        formatter: (cellContent: any, row: { enabled: any; }) => {
+        formatter: (cellContent: any, row: any) => {
             const color = row.enabled ? "outline-danger" : "outline-primary"
             const text = row.enabled ? "disable" : "enable"
             return (
-                <Button variant={color} size={"sm"}> {text}</Button>
+                <Button variant={color} size={"sm"} onClick={() => modalDisableAccount(row.id)}> {text}</Button>
             );
         }
     }];
@@ -85,8 +156,13 @@ export default function ListAccount() {
             <div className={"w-100 mx-auto m-5"}>
                 <h2 className="p-2 text-center">Account list</h2>
                 <div className={"w-75 mx-auto"}>
-                    <BootstrapTable keyField='id' data={products} columns={columns}/>
+                    <BootstrapTable keyField='id'
+                        data={data}
+                        columns={bootstrapTable2Columns}
+                        expandRow={bootstrapTable2ExpandRowListener}
+                    />
                 </div>
+
             </div>
         </AppBar>
     );
